ee8dc0f54ceee64d87a89872c1ce378f6aadda9a
yaaka <hrk.yszwz@gmail.com>
2023-02-11 19:59:05 +0900

    abc086_a

---
+++ src/abc086/product/Main.java
@@ -0,0 +1,11 @@
+package abc086.product;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 掛算
+        System.out.println((sc.nextInt() * sc.nextInt()) % 2 == 0 ? "Even" : "Odd");
+    }
+}
