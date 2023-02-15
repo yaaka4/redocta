6a7830678398e657ad3e0f9ebd3044efcf25fe87
yaaka <hrk.yszwz@gmail.com>
2023-02-16 01:13:04 +0900

    abc085_c

---
+++ src/abc085/c/Main.java
@@ -0,0 +1,28 @@
+package abc085.c;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+
+        final var n = sc.nextInt();
+        final var y = sc.nextInt();
+
+        var result = "-1 -1 -1";
+        Loop:
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n - i; j++) {
+                final var amount = 10000 * i + 5000 * j + 1000 * (n - i - j);
+                if (y == amount) {
+                    result = i + " " + j + " " + (n - i - j);
+                    break Loop;
+                }
+            }
+        }
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
