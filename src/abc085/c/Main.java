ec9caf072d0d281f3fb9d25ee61ac40109a0e810
yaaka <hrk.yszwz@gmail.com>
2023-02-17 01:15:01 +0900

    abc085_c

--- src/abc085/c/Main.java
+++ src/abc085/c/Main.java
@@ -13,6 +13,7 @@ public class Main {
         Loop:
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= n - i; j++) {
+                // 3つの変数を動かすのではなく２つに絞る
                 final var amount = 10000 * i + 5000 * j + 1000 * (n - i - j);
                 if (y == amount) {
                     result = i + " " + j + " " + (n - i - j);
